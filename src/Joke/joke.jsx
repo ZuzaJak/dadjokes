2f1e58feb95396369720f58426c437d28bed8c67
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-21 14:50:57 +0100

    Joke - úkol 15

---
+++ src/Joke/joke.jsx
@@ -0,0 +1,11 @@
+/* ÚKOL 15: Vytvořte komponentu Joke, která zobrazuje jeden vtip. Tato komponenta bude sídlit ve vlastní složce se všemi potřebnými CSS styly a obrázky. Komponenta Joke nechť má pět props: userId, userName, text, likes, dislikes.
+V hlavním souboru projektu importujte pole objektů ze souboru jokes.js. Zobrazte každý vtip z pole pomocí komponenty Joke.
+Výsledná aplikace by měla zobrazovat seznam všech vtipů se správnými uživateli a počty likeů. Tlačítka u každého vtipu by měla fungovat tak, že mění počty likeů uvnitř každé jednotlivé komponenty. */
+import React from 'react';
+import './style.css';
+
+const Joke = () => {
+  return <div></div>;
+};
+
+export default Joke;
