5e5affec92099b0f183d3173bc474faa6ba1a4eb
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-21 14:32:39 +0100

    funguje! - přidány funkce Like a Dislike, stavy

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-# Project Starters
+# Závěrečný test DA Web, Czechitas
 
-Základní nastavení projektu pro Reactovou webovou aplikaci s Webpackem.
-
-## Vývoj
-
-1. Nainstalujte závislosti příkazem `npm install`.
-1. Spusťte watch s dev serverem příkazem `npm run start`.
-
-## Produkční sestavení
-
-Pro produkční sestavení pusťte příkaz `npm run build`.
+Úkoly 14 a 15.
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './index.html';
 import './style.css';
 import asset from './assets/img/user01.png';
 
 const App = () => {
+  const [upLikes, setUpLikes] = useState(0);
+  const [downLikes, setDownLikes] = useState(0);
+
+  const Like = () => {
+    setUpLikes(upLikes + 1);
+  };
+  const Dislike = () => {
+    setDownLikes(downLikes + 1);
+  };
   return (
     <div className="container">
       <div className="joke">
@@ -20,13 +29,22 @@ const App = () => {
           </p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
-          <span id="likes-up" className="likes-count likes-count--up"></span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
-          <span
-            id="likes-down"
-            className="likes-count likes-count--down"
-          ></span>
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={Like}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {upLikes}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={Dislike}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {downLikes}
+          </span>
         </div>
       </div>
     </div>
